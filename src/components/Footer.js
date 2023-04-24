763af4e5eaab0ca4091ebb58e0128aa3ffe7254d
white-knight666 <aw.sh2000@hotmail.com>
2023-04-24 02:17:33 +0200

    fixing navbar SVG icon color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,25 +10,27 @@ export const Footer = () => {
   return (
     <footer className="footer">
       <Container>
-      
         <Row className="align-items-center container2">
           {/* <MailchimpForm /> */}
-          
           <Col size={12} sm={6}>
             {/* <img src={logo} alt="Logo" /> */}
             <p>Copyright 2022. All Rights Reserved</p>
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end container2">
             <div className="social-icon">
-              <a href="https://www.linkedin.com/in/mohamed-rajab-301581272/"><img src={navIcon1} alt="Icon" /></a>
-              <a href="tel:+31685085291"><img src={navIcon2} alt="Icon" /></a>
-              <a href="https://github.com/white-knight666"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://www.linkedin.com/in/mohamed-rajab-301581272/">
+                <img src={navIcon1} alt="Icon" />
+              </a>
+              <a href="tel:+31685085291">
+                <img src={navIcon2} alt="Icon" />
+              </a>
+              <a href="https://github.com/white-knight666">
+                <img src={navIcon3} alt="Icon" />
+              </a>
             </div>
-            
           </Col>
         </Row>
-      
       </Container>
     </footer>
-  )
-}
+  );
+};
